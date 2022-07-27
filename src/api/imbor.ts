5449904dc7df1562dad5bd60904c0fc6bbc1eef0
bartwr <mail@bartroorda.nl>
2022-07-27 20:46:15 +0200

    Editing queries so we get the right info

--- src/api/imbor.ts
+++ src/api/imbor.ts
@@ -28,13 +28,20 @@ export const getFysicalObjects = async (): Promise<any> => {
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX dash: <http://datashapes.org/dash#>
 
-    SELECT ?classURI ?label ?subClassOf WHERE {
+    SELECT
+      ?classURI ?label ?subClassOf
+    WHERE {
       ?classURI rdf:type rdfs:Class ;
-      dash:abstract false ;
-      rdfs:subClassOf* reeelOfRuimtelijk;
-      skos:prefLabel ?label .
+                dash:abstract false ;
+                rdfs:subClassOf* ?reeelOfRuimtelijk;
+                rdfs:subClassOf ?subClassOf;
+                skos:prefLabel ?label .
 
-      FILTER ( ?reeelOfRuimtelijk IN ( nen2660:RealObject , nen2660:FunctionalSpace ) )
+      FILTER (
+        ?reeelOfRuimtelijk IN (
+          nen2660:RealObject , nen2660:FunctionalSpace
+        )
+      )
     }
     ORDER BY STR(?label)
    `
@@ -58,17 +65,22 @@ export const getAttributesForClass = async (classUri: string): Promise<any> => {
     PREFIX dash: <http://datashapes.org/dash#>
     PREFIX sh: <http://www.w3.org/ns/shacl#>
 
-    select ?attributeURI ?attributeLabel ?attributeDefinition WHERE {
+    # Inspiration: https://github.com/Stichting-CROW/ldp-queries/blob/main/src/public/IMBOR2022_Attributen_per_Klasse.rq#L38
+    SELECT
+      ?entry_iri ?entry_text ?entry_definition ?group_iri
+    WHERE {
         <${classUri}> rdf:type rdfs:Class ;
-        dash:abstract false ;
-        rdfs:subClassOf* nen2660:PhysicalObject .
+                      dash:abstract false ;
+                      rdfs:subClassOf ?group_iri ;
+                      rdfs:subClassOf* nen2660:PhysicalObject .
 
-        <${classUri}> sh:property/sh:path ?attributeURI .
+        <${classUri}> sh:property/sh:path ?entry_iri .
 
-        OPTIONAL { ?attributeURI skos:prefLabel ?attributeLabel . }
-        OPTIONAL { ?attributeURI skos:definition ?attributeDefinition . }
+        OPTIONAL { ?attributeURI skos:prefLabel ?entry_text . }
+        OPTIONAL { ?attributeURI skos:definition ?entry_definition . }
         OPTIONAL { ?attribuut nen2660:hasQuantityKind/skos:prefLabel ?attributQuantityKind . }
     }
+
    `
 
   try{
