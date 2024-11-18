c7e69137a560f106820a2b66e22df1c09ae467e5
bartwr <mail@bartroorda.nl>
2024-11-18 20:27:25 +0100

    Fix favicon path

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,15 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Voorbeeldimplementatie van software die werkt op basis van NEN 2660-2:2022"
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/icon/icon-imbor-app.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+        <link rel="apple-touch-icon" href="./icon/icon-imbor-app.png" />
+        <link rel="manifest" href="./manifest.json" />
         <title>Demo NEN2660 Beheer app (asset management applicatie)</title>
         <link href="https://unpkg.com/maplibre-gl@2.1.9/dist/maplibre-gl.css" rel="stylesheet" />
       </head>
