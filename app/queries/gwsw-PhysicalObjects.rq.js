6c4adcfa67f7407413f101f90e6418eab2656d32
bartwr <mail@bartroorda.nl>
2024-11-18 20:08:20 +0100

    Fix query: Get GWSW Physical Objects

--- app/queries/gwsw-PhysicalObjects.rq.js
+++ app/queries/gwsw-PhysicalObjects.rq.js
@@ -4,28 +4,21 @@ export const query = () => `
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-  PREFIX gwsw: <http://data.gwsw.nl/1.5/totaal/>
+  PREFIX gwsw: <http://data.gwsw.nl/1.6/totaal/>
   PREFIX sh: <http://www.w3.org/ns/shacl#>
   PREFIX dash: <http://datashapes.org/dash#>
 
   SELECT
     ?classURI $label ?subClassOf
   WHERE {
-    # selecteer klassen die subklasse zijn van FysiekObject
+    # selecteer klassen die subklasse zijn van FysiekObject OF Ruimte
     {?classURI rdfs:subClassOf* gwsw:FysiekObject.}
     UNION
-    {?classURI rdfs:subClassOf* nen2660:SpatialRegion.}
+    {?classURI rdfs:subClassOf* gwsw:Ruimte.}
 
     # Selecteer het label
     ?classURI
       rdfs:label ?label .
-
-    #?classURI
-
-    # ... die niet abstract zijn
-      #dash:abstract false ;
-    # en selecteer het label
-      #skos:prefLabel ?label .
 
   } limit 99999
 `
